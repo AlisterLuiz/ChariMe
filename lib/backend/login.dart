a5f641749b2100240411b7c2b6cea507dbb975fc
Shivam Arora <shivam.aroraa@icloud.com>
2020-07-13 21:09:57 +0400

    test functions for encrypting the password

---
+++ lib/backend/login.dart
@@ -0,0 +1,6 @@
+import 'package:dbcrypt/dbcrypt.dart';
+import 'package:mysql1/mysql1.dart';
+
+//bool verify(String username, String plainPassword) {
+//  var isCorrect = new DBCrypt().checkpw(plainPassword, hashed);
+//}
---
+++ lib/backend/register.dart
@@ -0,0 +1,35 @@
+import 'package:dbcrypt/dbcrypt.dart';
+import 'package:mysql1/mysql1.dart';
+
+
+
+Future<void> dbConnect() async {
+
+  var settings = new ConnectionSettings(
+      host: 'localhost',
+      port: 3306,
+      user: 'peanut',
+      password: 'willywonka',
+      db: 'data'
+  );
+  var conn = await MySqlConnection.connect(settings);
+
+
+
+}
+
+
+
+bool register(String firstName, String emailID, String password) {
+
+}
+
+String hashPassword(String plainPassword) {
+  var hashedPassword = new DBCrypt().hashpw(plainPassword, new DBCrypt().gensalt());
+  print(hashedPassword);
+  return hashedPassword;
+}
+
+
+
+
