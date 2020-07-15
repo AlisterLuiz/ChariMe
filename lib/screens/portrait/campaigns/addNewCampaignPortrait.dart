5e6676c8d1befe8596babac13efc5d6730d0b598
Alister Luiz <alisterluiz@gmail.com>
2020-07-15 14:44:47 +0400

    Minor Changes:

--- lib/screens/portrait/campaigns/addNewCampaignPortrait.dart
+++ lib/screens/portrait/campaigns/addNewCampaignPortrait.dart
@@ -39,7 +39,7 @@ class _AddNewCampaignPortraitState extends State<AddNewCampaignPortrait> {
                   },
                 ),
                 Expanded(
-                  flex: 8,
+                  flex: 10,
                   child: ListView(
                     children: [
                       Center(
